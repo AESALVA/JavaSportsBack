3617529fde626b41dd6ae257db9ff5cd71d93b87
AESalva <eduardo_salva@hotmail.com>
2022-11-17 23:55:26 -0300

    continue building structures

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+let cors = require('cors')
+const MOCK = require('./Mocks/Mocks');
+app.use(cors());
 app.use(express.json());
 require('dotenv').config();
 
@@ -9,47 +12,23 @@ app.listen(PORT, () => {
     console.log('Server running on port ' + PORT);
   });
 
-const MOCK = [
-    {
-        id: "1",
-        categories: "football",
-        title:
-          "Gabriel Jesus is not the perfect striker but..",
-        img: "/img/imgSection.webp",
-        imgTitle:
-          "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        description:
-          "Five-nil. But then, Manchester City just look so strong at the moment. Still: five-nil. But that injury to Bukayo Saka, how much of a blow could that be? Anyway, it was five-nil. But those Nottingham Forest chances, the Jesse Lingard shot where Gabriel Magalhães basically passes it straight to him, one day those are going to cost you. And yet, check the scoreboard, girls and boys: five-nil. It is the eternal gift of Arsenal fans to be able to identify the potential anxiety in virtually any situation, however favourable or triumphant. Pleasure is simply misery deferred. Most people look for crumbs of comfort; Arsenal fans look for crumbs of concern, scan the fixture list for oncoming hazards, scan every horizon for grey clouds. And so, even a routine and rampant 5-0 victory comes with its own quantum of unease.",
-        imgTwo: "/img/imgSection2.webp",
-        synopsis:
-          "Of course, as anyone who has seen Jesus play will be able to tell you, this is not all you get. One of the reasons it feels unfair judging Jesus by the output of a conventional striker is that his input is not that of a conventional striker. Of his 72 touches, only 16 came in the Forest penalty area. Thirteen were in his own half. The vast majority, in fact, were on the left wing, with another little cluster around the left edge of the centre circle. He attempted more tackles than any other Arsenal player. Jesus is, in effect, a ball-winning wide midfielder who does striking for a little extra cash.",
-      },
-      {
-        id: "2",
-        categories: "football",
-        title:
-          "Gabriel Jesus is not the perfect striker but..",
-        img: "/img/imgSection.webp",
-        imgTitle:
-          "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        description:
-          "Five-nil. But then, Manchester City just look so strong at the moment. Still: five-nil. But that injury to Bukayo Saka, how much of a blow could that be? Anyway, it was five-nil. But those Nottingham Forest chances, the Jesse Lingard shot where Gabriel Magalhães basically passes it straight to him, one day those are going to cost you. And yet, check the scoreboard, girls and boys: five-nil. It is the eternal gift of Arsenal fans to be able to identify the potential anxiety in virtually any situation, however favourable or triumphant. Pleasure is simply misery deferred. Most people look for crumbs of comfort; Arsenal fans look for crumbs of concern, scan the fixture list for oncoming hazards, scan every horizon for grey clouds. And so, even a routine and rampant 5-0 victory comes with its own quantum of unease.",
-        imgTwo: "/img/imgSection2.webp",
-        synopsis:
-          "Of course, as anyone who has seen Jesus play will be able to tell you, this is not all you get. One of the reasons it feels unfair judging Jesus by the output of a conventional striker is that his input is not that of a conventional striker. Of his 72 touches, only 16 came in the Forest penalty area. Thirteen were in his own half. The vast majority, in fact, were on the left wing, with another little cluster around the left edge of the centre circle. He attempted more tackles than any other Arsenal player. Jesus is, in effect, a ball-winning wide midfielder who does striking for a little extra cash.",
-      }
-];
 
 app.get('/articles', (req, res)=>{
     const allArticles = MOCK;
     console.log('get');
-    res.send(allArticles);
+    res.json(allArticles);
 })
+
+app.post('/comments', (req, res)=>{
+  const { body } = req;
+  console.log('post comments');
+  res.status(200).json(body)
+})  
 
 app.get('/article/:id',(req,res)=>{
     const { id } = req.params;
 const article = MOCK.find(a=>a.id === id);
-res.status(200).send(article);
+res.status(200).json(article);
 })
 
 app.route('/test').get((req,res)=>{
@@ -57,5 +36,5 @@ app.route('/test').get((req,res)=>{
     console.log('get');
     res.send(allArticles);
 }).post((req, res)=>{
-res.send(req.body);
+res.json(req.body);
 })
